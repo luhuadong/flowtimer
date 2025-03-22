a7f9bc5f465a9ade455368d839ed6d56e88f1289
luhuadong <luhuadong@163.com>
2025-03-23 03:59:50 +0800

    release: v0.1.0

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="flowtimer",
     version="0.1.0",
+    author="luhuadong",
+    author_email="luhuadong@163.com",
+    description="A terminal Pomodoro timer with productivity stats",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/luhuadong/flowtimer",
     packages=find_packages(),
     install_requires=[
         "click>=8.0",
